f6a84138552c872801c2b3dcd50e4ff7817f047a
jsbry <jsbry.biz@gmail.com>
2018-07-07 02:42:21 +0900

    update: cmd config test

--- cmd_config_test.go
+++ cmd_config_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func TestConfigScan(t *testing.T) {
+	keyFile, dataFile, filteringCommand, unprotectiveCommands, err := configScan()
+	if err != nil {
+		t.Error(err)
+	}
+	if keyFile != "~/.ssh/id_rsa" {
+		t.Error(err)
+	}
+	if dataFile != "~/.local/share/spwd/data.dat" {
+		t.Error(err)
+	}
+	if filteringCommand != "peco" {
+		t.Error(err)
+	}
+	if len(unprotectiveCommands) != 0 {
+		t.Error(err)
+	}
+}
+
 func TestCmdConfig(t *testing.T) {
 	testConfig := Config{
 		KeyFile:              filepath.Join("testdata", "key_file"),
